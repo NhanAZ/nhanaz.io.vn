0cb3d33b38b24eef0be5129d9eced0fc5f96bf3e
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2024-06-27 05:45:08 +0700

    Revert "Update docusaurus.config.js"
    This reverts commit 76e82d7ce787a1f6cb85a81d109400d5bae8a1ab.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/NhanAZ/AZDocs/tree/main/',
+						'https://github.com/${organizationName}/${projectName}/tree/main/',
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/NhanAZ/AZDocs/tree/main/',
+						'https://github.com/${organizationName}/${projectName}/tree/main/',
 				},
 				theme: {
 					customCss: './src/css/custom.css',
