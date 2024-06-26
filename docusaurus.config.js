46da22a0c93e61d2f2077ea4b9af906a2ca4696a
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2024-06-27 04:33:35 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://nhanaz.tech',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/${projectName}/',
+  baseUrl: '/AZDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
